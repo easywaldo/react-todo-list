36997b3c6d8542fe8ba40b695006d24391ecef5d
easywaldo <acetious@gmail.com>
2018-11-27 23:50:38 +0900

    컴포넌트 구성하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import TodoListTemplates from './components/TodoListTemplates';
+import './index.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
-    )
+      <TodoListTemplates>
+        템플릿 완성
+      </TodoListTemplates>
+    );
   }
 }
 
