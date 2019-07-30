c437372686a37d1eec424a1238b78c54b6732aa2
easywaldoji <easywaldoji@gmail.com>
2019-07-31 07:37:08 +0900

    Updates

---
+++ precache-manifest.1f063730544ca9af730fa29a465c92fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c69fbd2ddfb64582185",
+    "url": "/react-todo-list/static/css/main.7b816bf9.chunk.css"
+  },
+  {
+    "revision": "5c69fbd2ddfb64582185",
+    "url": "/react-todo-list/static/js/main.5c69fbd2.chunk.js"
+  },
+  {
+    "revision": "a6b9e825a9e1a0fa0cf5",
+    "url": "/react-todo-list/static/js/1.a6b9e825.chunk.js"
+  },
+  {
+    "revision": "9061ff665ca8ca5bb17c",
+    "url": "/react-todo-list/static/js/runtime~main.9061ff66.js"
+  },
+  {
+    "revision": "9d421058a9a8e3e30622c409430eb052",
+    "url": "/react-todo-list/index.html"
+  }
+];
